bbaad87918bd26b08b43877f3873250d8b23aac3
Arrow Rowe <arrowrowe@gmail.com>
2015-06-12 10:45:28 +0800

    Add for-let-of note: block prevents error?

--- try-let.js
+++ try-let.js
@@ -1,9 +1,25 @@
+/*
 try {
     for (let prime of [2, 3, 5, 7]) {
         console.log(prime);
     }
-    console.log(prime);
+    console.log('Last prime:', prime);
 } catch (err) {
     console.log('Run into error', err);
+}
+ */
+
+/*
+ * It is very strange...
+ * In the code above, the try-catch is necessary.
+ * Remove line 2, 7, 8, 9, error occurs.
+ * However, the code following is correct as well!
+ */
+
+{
+    for (let prime of [2, 3, 5, 7]) {
+        console.log(prime);
+    }
+    console.log('Last prime:', prime);
 }
 
